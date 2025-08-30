36277036f3ce2b0786975001118c3d2903b0e544
Mazhar <mi.sabbir420@gmail.com>
2025-08-30 18:02:08 +0600

    hotline copy js code adding

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-function abc(){
- console.log(arguments)
-}
+console.log("connected");
 
-abc("a","b", 'c')
+document.querySelectorAll(".heart-icon").forEach(function (heart) {
+  heart.addEventListener("click", function () {
+    let heartNumber = document.getElementById("heart-count");
+    let heartFeildNumber = heartNumber.innerText;
+    let heartFeildNumberConvert = parseInt(heartFeildNumber);
 
-function sortArr(arr){
-    arr.sort();
-    console.log(arr)
-    return arr;
-}
+    heartFeildNumberConvert++;
 
-sortArr([7,4,2,8,9,0,1])
+    heartNumber.innerText = heartFeildNumberConvert;
+
+    // console.log(heartNumber)
+  });
+});
+
+document.querySelectorAll(".cp-btn").forEach(function (copyBtn) {
+  copyBtn.addEventListener("click", function () {
+    let copyElement = document.getElementById("copyfeild");
+    let copyElementNumber = copyElement.innerText;
+    let copyNumberConverted = parseInt(copyElementNumber);
+    console.log(copyNumberConverted);
+    copyNumberConverted++;
+    copyElement.innerText = copyNumberConverted;
+
+    const cardBody = copyBtn.closest(".card-body");
+    const numberElement = cardBody.querySelector(".hot-line-text");
+    const copyText = numberElement.innerText;
+    console.log(copyText)
+  });
+});
