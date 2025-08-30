dd6efab6d84114927f467eba9a25568cd0ed872c
Mazhar <mi.sabbir420@gmail.com>
2025-08-30 20:59:13 +0600

    calling function done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-console.log("connected");
-
+// click on heart icon, heart count increase
 document.querySelectorAll(".heart-icon").forEach(function (heart) {
   heart.addEventListener("click", function () {
     let heartNumber = document.getElementById("heart-count");
@@ -14,14 +13,15 @@ document.querySelectorAll(".heart-icon").forEach(function (heart) {
   });
 });
 
+// copy text update copy count, copy on clipboard and show alert
 document.querySelectorAll(".cp-btn").forEach(function (copyBtn) {
   copyBtn.addEventListener("click", function () {
-    let copyElement = document.getElementById("copyfeild");
-    let copyElementNumber = copyElement.innerText;
-    let copyNumberConverted = parseInt(copyElementNumber);
-    console.log(copyNumberConverted);
-    copyNumberConverted++;
-    copyElement.innerText = copyNumberConverted;
+    // let copyElement = document.getElementById("copyfeild");
+    // let copyElementNumber = copyElement.innerText;
+    // let copyNumberConverted = parseInt(copyElementNumber);
+    // console.log(copyNumberConverted);
+    // // copyNumberConverted++;
+    // copyElement.innerText = copyNumberConverted;
 
     const cardBody = copyBtn.closest(".card-body");
     const numberElement = cardBody.querySelector(".hot-line-text");
@@ -31,7 +31,6 @@ document.querySelectorAll(".cp-btn").forEach(function (copyBtn) {
       .then(() => {
         console.log(`Copied: ${copyText}`);
 
-        // 4. Increment global copy counter
         const copyCounter = document.getElementById("copyfeild");
         copyCounter.innerText = parseInt(copyCounter.innerText) + 1;
       })
@@ -40,3 +39,27 @@ document.querySelectorAll(".cp-btn").forEach(function (copyBtn) {
     alert(`Copied: ${copyText}`);
   });
 });
+
+// call button, alert wih service name & number, cut 20 in coin every click.
+document.querySelectorAll('.call-btn').forEach(function (callBtn) {
+  callBtn.addEventListener('click', function () {
+    let coin = document.getElementById("coin-number");
+    let coinNummber = coin.innerText;
+    let coinNumberConverted = parseInt(coinNummber);
+    console.log(coinNumberConverted)
+
+    if (coinNumberConverted < 20) {
+      alert(`No Coin 😓 Available`)
+      return ;
+    }
+    const cardBody = callBtn.closest(".card-body");
+    const serviceName = cardBody.querySelector(".service-name").innerText;
+    const serviceNumber  = cardBody.querySelector(".hot-line-text").innerText;
+    console.log(serviceNumber,serviceName);
+
+    alert(`📞 Calling: ${ serviceName} ${ serviceNumber}...`)
+    let newCoinAmount = coinNumberConverted - 20;
+    coin.innerText = newCoinAmount;
+    console.log(newCoinAmount);
+  })
+})
