4e575fee0be6692d4878a3b971130f8909558658
Mazhar <mi.sabbir420@gmail.com>
2025-08-31 16:21:09 +0600

    added call histort section padding min wigth and max wight

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ document.querySelectorAll('.call-btn').forEach(function (callBtn) {
     for (const data of callHistryData) {
       const div = document.createElement('div')
       div.innerHTML = `
-       <div class=" bg-[#F2F2F2] ml-2 sm:mx-3 mb-2 w-[180px] flex flex-col  sm:flex-row items-start rounded-lg p-2 justify-between sm:items-center ">
+       <div class=" bg-[#F2F2F2] ml-2 sm:mx-3 mb-2 max-w-[220px] min-w-[180px] flex flex-col  sm:flex-row items-start rounded-lg p-2 justify-between sm:items-center ">
                     <div>
                         <p class="font-bold">${data.name}</p>
                         <p>${data.number}</p>
